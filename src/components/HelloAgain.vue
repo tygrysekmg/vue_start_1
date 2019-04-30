b2b93513ee2098c03605c5eab6ac23696a53944d
Grzegorz <w.luk1972@wp.pl>
2019-04-30 18:58:17 +0200

    first commit

---
+++ src/components/HelloAgain.vue
@@ -0,0 +1,19 @@
+<template>
+ <p class="hello-again">
+ This is Grzegorza component.
+ </p>
+</template>
+
+<script>
+export default {
+ name: 'HelloAgain'
+}
+</script>
+
+<style scoped>
+p {
+ font-size: 16px;
+ text-align: center;
+ color: tomato;
+}
+</style>
